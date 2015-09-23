a9e83caeb5e07664544e92f05f046088f071a93a
gera <semikozovgerman@gmail.com>
2015-09-23 11:41:09 +0300

    added menu-toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-
+    $('#first-item').on('click', function() {
+        $('#first-item_menu').toggleClass('hidden-xs');
+    });
+    $('#second-item').on('click', function() {
+        $('#second-item_menu').toggleClass('hidden-xs');
+    });
+    $('#third-item').on('click', function() {
+        $('#third-item_menu').toggleClass('hidden-xs');
+    })
 });
 
 $(window).load(function() {
